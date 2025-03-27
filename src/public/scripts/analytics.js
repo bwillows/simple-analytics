99e61be914032ea8509e37a831e1e286abbe7380
bwillows <152449409+bwillows@users.noreply.github.com>
2025-03-27 11:34:49 -0700

    Update analytics.js

--- src/public/scripts/analytics.js
+++ src/public/scripts/analytics.js
@@ -1,13 +1,35 @@
+function random(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
 let load_time = new Date();
 let view_duration = 0;
-let session = random(7);
 
-fetch("/analytics/init")
+let session;
+let session_start_token;
+
+fetch("/analytics/init/")
         .then((response) => response.text())
-        .then(() => {
-            
+        .then((res) => {
+            session = res;
         });
 
 setTimeout(function() {
     view_duration++;
 }, 1000);
+
+window.onbeforeunload = function () {
+    fetch("/analytics/terminate/" + "?session=" + session + "?view-duration=" + view_duration)
+        .then((response) => response.text())
+        .then((res) => {
+
+        });
+};
